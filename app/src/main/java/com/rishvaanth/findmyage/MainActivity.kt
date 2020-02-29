650a51d31b614f3653ebebfac812aff30a039f9b
rishvaanth13 <rishvaanth.t@ril.com>
2020-03-01 03:58:34 +0530

    Implemented the Logic

--- app/src/main/java/com/rishvaanth/findmyage/MainActivity.kt
+++ app/src/main/java/com/rishvaanth/findmyage/MainActivity.kt
@@ -2,11 +2,23 @@ package com.rishvaanth.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener{
+            buttonClick()
+        }
+    }
+
+    fun buttonClick(){
+        val DOB = Integer.parseInt(editDateofBirth.text.toString())
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        val userAgeinYears = currentYear-DOB
+        result.text = "You are $userAgeinYears"
     }
 }
